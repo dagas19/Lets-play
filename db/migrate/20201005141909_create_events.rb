5a8de921bfda6e15d5fc97e63c5b129309f9be7b
DS <dianakondro@gmail.com>
2020-10-05 16:24:25 +0200

    fixed models

--- db/migrate/20201005141909_create_events.rb
+++ db/migrate/20201005141909_create_events.rb
@@ -2,20 +2,16 @@ class CreateEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :events do |t|
       t.string :title
-      t.integer :game_id
-      t.integer :venue_id
       t.date :date
       t.integer :spots
       t.text :description
-      t.integer :gender_id
       t.string :experience_level
-      t.integer :user_id
       t.integer :min_age
       t.integer :max_age
-      t.references :user, null: false, foreign_key: true
-      t.references :venue, null: false, foreign_key: true
       t.references :game, null: false, foreign_key: true
+      t.references :venue, null: false, foreign_key: true
       t.references :gender, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20201005142241_create_messages.rb
+++ db/migrate/20201005142241_create_messages.rb
@@ -2,8 +2,8 @@ class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
       t.text :content
-      t.integer :event_id
-      t.integer :user_id
+      t.date :date
+      t.references :event, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
