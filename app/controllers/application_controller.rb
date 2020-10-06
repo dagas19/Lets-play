7dad1d8c842dcececeaf74c00b1c74cb43c27f0f
DS <dianakondro@gmail.com>
2020-10-06 19:12:57 +0200

    card-box fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  
   
   include Pundit
 
@@ -21,7 +21,4 @@ class ApplicationController < ActionController::Base
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[nickname gender_id age])
-  end
 end
