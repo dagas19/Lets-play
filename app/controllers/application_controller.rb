6b38ba088541bbd92fdf61b45e33fe17c70669ed
Stian Johansen <stian.a.johansen@gmail.com>
2020-10-06 15:55:53 +0200

    Added new fields to form and updated application controller accordingly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  include pundit
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  include Pundit
 
   # Pundit: white-list approach.
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
@@ -17,5 +19,9 @@ class ApplicationController < ActionController::Base
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[nickname gender_id age])
   end
 end
