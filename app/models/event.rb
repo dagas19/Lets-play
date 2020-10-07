99ccc2cb6bb5a823f82a7069a216eb5ff5b4a360
John El Basha <johnelbasha@gmail.com>
2020-10-07 09:39:21 +0100

    update some validations in line with Yair's feedback provided yesterday

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,9 +3,14 @@ class Event < ApplicationRecord
   belongs_to :venue
   belongs_to :gender
   belongs_to :user
-  has_many :users
+  has_many :registrations, dependent: :destroy
+  has_many :users, through: :registrations
   has_many :messages
 
   validates :title, presence: true
   validates :spots, presence: true
+
+  def participants
+    [user] + users
+  end
 end
