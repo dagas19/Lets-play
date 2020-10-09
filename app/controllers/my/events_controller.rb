96db75b5d68ae859694386649a060833597ca9aa
John El Basha <johnelbasha@gmail.com>
2020-10-09 11:38:21 +0100

    combined events and participations into @all_events

--- app/controllers/my/events_controller.rb
+++ app/controllers/my/events_controller.rb
@@ -3,5 +3,20 @@ class My::EventsController < ApplicationController
   def index
     @events = policy_scope([:my, Event.where(user: current_user)])
     @participations = policy_scope([:my, Registration.where(user: current_user)])
+    group_events(@events, @participations)
+    raise
+  end
+
+  private
+
+  def group_events(events1, events2)
+    @all_events = []
+    events1.each do |event|
+      @all_events <<  event
+    end
+    events2.each do |par|
+      @all_events <<  par
+    end
+    @all_events
   end
 end
