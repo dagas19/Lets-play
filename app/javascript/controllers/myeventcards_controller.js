fe5d78a2783e79e0f728a91ffc4745a3e6504b9a
John El Basha <johnelbasha@gmail.com>
2020-10-13 10:12:11 +0100

    further improvements in js

--- app/javascript/controllers/myeventcards_controller.js
+++ app/javascript/controllers/myeventcards_controller.js
@@ -1,27 +1,32 @@
+// create element selectors
 const button_myEvents = document.querySelector('#my-events')
 const button_eventsJoined = document.querySelector('#events-joined')
-
-console.log('this js file that john is writing actually works')
-
-
+const dashboardEvents = document.querySelectorAll('.dashboardEvent')
 const cards = document.querySelectorAll('.event-card')
 
+dashboardEvents.forEach(item => {
+  console.log(item.dataset.eventid)
+})
 
-console.log(cards[0].dataset.myevent)
 
+cards.forEach(card => {
+  card.addEventListener('click', (event) => {
+    console.log('you clicked on a card')
+    console.log(card)
+    updateDasboardWithCard(card)
+  })
+})
 
 
 button_myEvents.addEventListener('click', (event) => {
   console.log('you clicked on my events button')
   toggleVisibleCardsMyEvents(cards);
-
 })
 
 button_eventsJoined.addEventListener('click', (event) => {
   console.log('you clicked on joined events button')
   toggleVisibleCardsOtherEvents(cards);
 })
-
 
 function toggleVisibleCardsMyEvents(cards) {
   console.log('function triggered');
@@ -51,8 +56,14 @@ function toggleVisibleCardsOtherEvents(cards) {
 )
 }
 
+function updateDasboardWithCard(card) {
+  // extract the event id from the card
+  console.log('the event id of this card is')
+  console.log(card.dataset.eventid)
+  // go find/select the event id in the dashboard column with the same event id
 
-
+  // set that event id to normal display and to none for all the others.
+}
 
 
 
