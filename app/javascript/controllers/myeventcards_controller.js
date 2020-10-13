3008be547512cec0c12ec8a0a4f775467170e38b
John El Basha <johnelbasha@gmail.com>
2020-10-13 10:31:55 +0100

    improved front end

--- app/javascript/controllers/myeventcards_controller.js
+++ app/javascript/controllers/myeventcards_controller.js
@@ -4,27 +4,17 @@ const button_eventsJoined = document.querySelector('#events-joined')
 const dashboardEvents = document.querySelectorAll('.dashboardEvent')
 const cards = document.querySelectorAll('.event-card')
 
-dashboardEvents.forEach(item => {
-  console.log(item.dataset.eventid)
-})
-
-
 cards.forEach(card => {
   card.addEventListener('click', (event) => {
-    console.log('you clicked on a card')
-    console.log(card)
     updateDasboardWithCard(card)
   })
 })
 
-
 button_myEvents.addEventListener('click', (event) => {
-  console.log('you clicked on my events button')
   toggleVisibleCardsMyEvents(cards);
 })
 
 button_eventsJoined.addEventListener('click', (event) => {
-  console.log('you clicked on joined events button')
   toggleVisibleCardsOtherEvents(cards);
 })
 
@@ -32,10 +22,8 @@ function toggleVisibleCardsMyEvents(cards) {
   console.log('function triggered');
   cards.forEach(card => {
     if (card.dataset.myevent == "true") {
-      console.log('this card is yours')
       card.style.display = "block";
     } else {
-      console.log("this card isn't yours");
       card.style.display = "none";
     }
   }
@@ -43,13 +31,10 @@ function toggleVisibleCardsMyEvents(cards) {
 }
 
 function toggleVisibleCardsOtherEvents(cards) {
-  console.log('function triggered');
   cards.forEach(card => {
     if (card.dataset.myevent == "true") {
-      console.log('this card is yours')
       card.style.display = "none";
     } else {
-      console.log("this card isn't yours");
       card.style.display = "block";
     }
   }
@@ -57,12 +42,13 @@ function toggleVisibleCardsOtherEvents(cards) {
 }
 
 function updateDasboardWithCard(card) {
-  // extract the event id from the card
-  console.log('the event id of this card is')
-  console.log(card.dataset.eventid)
-  // go find/select the event id in the dashboard column with the same event id
-
-  // set that event id to normal display and to none for all the others.
+  dashboardEvents.forEach(event => {
+    if (event.dataset.eventid == card.dataset.eventid) {
+      event.style.display = "block";
+    } else {
+      event.style.display = "none";
+    }
+  })
 }
 
 
