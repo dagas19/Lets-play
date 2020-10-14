4f971e8f400a98f80af901af7dfd2518846411d3
John El Basha <johnelbasha@gmail.com>
2020-10-14 10:28:41 +0100

    chat windows now appear only when a card is clicked

--- app/javascript/controllers/chatWindowSelector_controller.js
+++ app/javascript/controllers/chatWindowSelector_controller.js
@@ -8,5 +8,22 @@ console.log('Trying to create html selectors for each chat window...');
 const eventChatWindows = document.querySelectorAll('.chat-container');
 console.log('Trying to print out the event ID from each of the chat window html elements...');
 eventChatWindows.forEach(window => console.log(window.dataset.eventid));
-console.log('trying to create the log of identifying every time someone clicks on a card...');
+console.log('trying to create the logic of identifying every time someone clicks on a card...');
+
+
+eventCards.forEach(card => {
+  card.addEventListener('click', (event) => {
+    console.log(event.currentTarget.dataset.eventid);
+    eventChatWindows.forEach(window => {
+      console.log(window.dataset.eventid)
+      if (event.currentTarget.dataset.eventid == window.dataset.eventid) {
+        window.classList.remove("hidden-chat");
+        window.classList.add("not-hidden-chat");
+      } else {
+        window.classList.remove("not-hidden-chat");
+        window.classList.add("hidden-chat");
+      }
+    })
+  })
+});
 
