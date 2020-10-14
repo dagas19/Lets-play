2e1ec848b0123bd28902256e86cf158d8d5a09d1
John El Basha <johnelbasha@gmail.com>
2020-10-14 15:24:05 +0100

    updates to chat now real-time

--- app/javascript/controllers/chatflow_controller.js
+++ app/javascript/controllers/chatflow_controller.js
@@ -1,23 +1,9 @@
-const currentUserMessages = document.querySelectorAll("div[data-mymessage='true']");
+const currentUserId = $(".chat-container").first().attr('data-my-user-id');
+const currentUserMessages = document.querySelectorAll("div[data-message-userid=\""+currentUserId+"\"]");
+
 currentUserMessages.forEach(message => {
   moveChatLogRight(message);
 })
-
-const chatInputField = document.querySelector(".chat-input-area__input");
-
-chatInputField.addEventListener('submit', (event) => {
-  const currentUserMessages = document.querySelectorAll("div[data-mymessage='true']");
-
-  currentUserMessages.forEach(message => {
-  moveChatLogRight(message);
-})
-})
-
-
-
-
-
-
 
 function moveChatLogRight(message) {
   message.classList.add("my-messages");
