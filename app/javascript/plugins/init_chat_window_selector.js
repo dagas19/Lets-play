dff48fbbe8422214e3dc01c2fa9a4e3f88aaa7ab
Dovydas Savickas <dagas199@gmail.com>
2020-10-15 18:23:52 +0200

    tabs funcionalkity

--- app/javascript/plugins/init_chat_window_selector.js
+++ app/javascript/plugins/init_chat_window_selector.js
@@ -1,21 +1,54 @@
 const initChatWindowSelector = () => {
   const eventCards = document.querySelectorAll('.event-card');
   const eventChatWindows = document.querySelectorAll('.chat-container');
+  window.chatShow = false
 
   eventCards.forEach(card => {
     card.addEventListener('click', (event) => {
-      eventChatWindows.forEach(window => {
-        if (event.currentTarget.dataset.eventid == window.dataset.eventid) {
-          window.classList.remove("hidden-chat");
-          window.classList.add("not-hidden-chat");
+      eventChatWindows.forEach(chatWindow => {
+        if (event.currentTarget.dataset.eventid == chatWindow.dataset.eventid && window.chatShow) {
+          chatWindow.classList.remove("hidden-chat");
+          chatWindow.classList.add("not-hidden-chat");
         } else {
-          window.classList.remove("not-hidden-chat");
-          window.classList.add("hidden-chat");
+          chatWindow.classList.remove("not-hidden-chat");
+          chatWindow.classList.add("hidden-chat");
         }
       })
     })
   });
 };
 
+const initInfoWindows = () => {
+  window.infoShow = true
+  const eventCards = document.querySelectorAll('.event-card');
+  const eventChatWindows = document.querySelectorAll('.chat-container');
 
-export { initChatWindowSelector };
+  eventCards.forEach(card => {
+    card.addEventListener('click', (event) => {
+      window.eventId = event.currentTarget.dataset.eventid
+      console.log(window.infoShow)
+      $(".event-info-2").addClass("d-none")
+      if(window.infoShow) {
+        $("#event-info-"+event.currentTarget.dataset.eventid).removeClass("d-none")
+      }
+    })
+  });
+}
+const initButtons = () => {
+  const chatButton = document.querySelector("#chat")
+  console.log(chatButton)
+  const infoButton = document.querySelector("#moreinfo")
+  chatButton.addEventListener("click", (event) => {
+    window.infoShow = false
+    window.chatShow = true
+    $("div.chat-container[data-eventid = '"+window.eventId+"']").removeClass("hidden-chat").addClass("not-hidden-chat")
+    $("#event-info-"+window.eventId).addClass("d-none")
+  })
+   infoButton.addEventListener("click", (event) => {
+    window.infoShow = true
+    window.chatShow = false
+    $("div.chat-container[data-eventid = '"+window.eventId+"']").addClass("hidden-chat").removeClass("not-hidden-chat")
+    $("#event-info-"+window.eventId).removeClass("d-none")
+  })
+}
+export { initChatWindowSelector, initInfoWindows, initButtons };
