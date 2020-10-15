9c896e05b42fcb9ccba171985ad5f0a1bc369612
John El Basha <johnelbasha@gmail.com>
2020-10-15 13:52:10 +0100

    fixed issue with chat loading on the left/right

--- app/javascript/channels/event_channel.js
+++ app/javascript/channels/event_channel.js
@@ -10,12 +10,7 @@ const initEventMessages = () => {
         {
           // called when data is broadcast in the cable
           received(data) {
-            const currentUserId = $(".chat-container").first().attr('data-my-user-id');
-            const currentUserMessages = document.querySelectorAll("div[data-message-userid=\""+currentUserId+"\"]");
 
-            currentUserMessages.forEach(message => {
-              moveChatLogRight(message);
-            })
             // Clear chat message in input
             container.querySelector(".chat-input-area__input")
               .value = ""
@@ -24,6 +19,13 @@ const initEventMessages = () => {
               .insertAdjacentHTML("beforeend", data)
             // scroll to chat end
             container.scrollTo(0, container.scrollHeight)
+
+            const currentUserId = $(".chat-container").first().attr('data-my-user-id');
+            const currentUserMessages = document.querySelectorAll("div[data-message-userid=\""+currentUserId+"\"]");
+
+            currentUserMessages.forEach(message => {
+              moveChatLogRight(message);
+            })
           },
         }
       )
