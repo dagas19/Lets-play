b2bcc48a92bcc6938d13cede0ece97197eae6438
John El Basha <johnelbasha@gmail.com>
2020-10-15 15:10:38 +0100

    now chat window has title

--- app/javascript/plugins/init_chat_window_selector.js
+++ app/javascript/plugins/init_chat_window_selector.js
@@ -1,6 +1,8 @@
 const initChatWindowSelector = () => {
   const eventCards = document.querySelectorAll('.event-card');
   const eventChatWindows = document.querySelectorAll('.chat-container');
+  const eventChatWindowTitles = document.querySelectorAll('.chat-title');
+  console.log(eventChatWindowTitles);
 
   eventCards.forEach(card => {
     card.addEventListener('click', (event) => {
@@ -11,6 +13,15 @@ const initChatWindowSelector = () => {
         } else {
           window.classList.remove("not-hidden-chat");
           window.classList.add("hidden-chat");
+        }
+      })
+      eventChatWindowTitles.forEach(title => {
+        if (event.currentTarget.dataset.eventid == title.dataset.eventid) {
+          title.classList.remove("hidden-chat");
+          title.classList.add("not-hidden-chat");
+        } else {
+          title.classList.remove("not-hidden-chat");
+          title.classList.add("hidden-chat");
         }
       })
     })
