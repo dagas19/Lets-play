39ec1eb69b34b8c3926c5c26ec19c9c2582ffd1c
Dovydas Savickas <dagas199@gmail.com>
2020-10-16 03:13:08 +0200

    added emoji

---
+++ app/javascript/controllers/emoji_controller.js
@@ -0,0 +1,14 @@
+import { EmojiButton } from '@joeattardi/emoji-button';
+
+const picker = new EmojiButton();
+const trigger = document.querySelector('.trigger');
+
+picker.on('emoji', selection => {
+  const area = document.querySelector('.chat-input-area__input');
+  console.log(area.value)
+  area.value += selection.emoji;
+  // `selection` object has an `emoji` property
+  // containing the selected emoji
+});
+
+trigger.addEventListener('click', () => picker.togglePicker(trigger));
